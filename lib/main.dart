fb479daeebdf6263bb4ccbd520482cc6c4d2b6b6
fernandojosecc <fernandojosecc@hotmail.com>
2025-03-18 14:23:32 -0700

    Adding all the files and code. I just added some basic widgets on my project

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:
+          false, //Is the strap at the corner that says DEBUG
+      title: 'Hola Mundo',
+      home: RecipeBook(),
+    );
+  }
+}
+
+class RecipeBook extends StatelessWidget {
+  const RecipeBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Recipe Book', style: TextStyle(color: Colors.white)),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 125,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(),
+                ),
+              ),
+              SizedBox(width: 26),
+              Column(
+                children: <Widget>[
+                  Text('Lasagna'),
+                  Text('Fernando C'),
+                  Container(height: 2, width: 75, color: Colors.blue),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
