4135d2d5ba86296b4f5dadb831ce49dbeda11b69
fernandojosecc <fernandojosecc@hotmail.com>
2025-03-21 12:51:33 -0700

    I created a new folder called screen who is gonna help us create a navigation tabs, we also created a Home_Screen file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_book/screens/home_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -21,36 +22,20 @@ class RecipeBook extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('Recipe Book', style: TextStyle(color: Colors.white)),
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 125,
-        child: Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                height: 125,
-                width: 100,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(),
-                ),
-              ),
-              SizedBox(width: 26),
-              Column(
-                children: <Widget>[
-                  Text('Lasagna'),
-                  Text('Fernando C'),
-                  Container(height: 2, width: 75, color: Colors.blue),
-                ],
-              ),
-            ],
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Recipe Book', style: TextStyle(color: Colors.white)),
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.red,
+            tabs: [Tab(icon: Icon(Icons.home), text: 'Home')],
           ),
         ),
+        body: TabBarView(children: [HomeScreen()]),
       ),
     );
   }
